e747f992d6daef7e15fbd31287e15ce2e370c9c9
Mars Anthony <145798641+manthony100@users.noreply.github.com>
2025-10-03 16:46:40 -0400

    Checkpoint Door Scene and Script
    Started working on the scene and script for the checkpoint door.
    - Disables collison on start
    - Will eventually enable collision "close door" once the player is a certain space above the door

---
+++ checkpoint_door.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+# tells Godot to assign colShp after the node (& children) is added to the scene tree
+@onready var colShp = $CollisionShape2D #
+
+func _ready():
+	colShp.disabled = true # Collision shape not active
+
+func _process(delta: float) -> void:
+	
+
+func _activateCollShape():
+	colShp.disabled = false # Collision shape is active
