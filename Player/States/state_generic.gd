c690ca6396f5988973e35fc4f67af4d514a8aec3
Mars Anthony <145798641+manthony100@users.noreply.github.com>
2025-11-14 14:36:02 -0500

    Copied Lucas's old files
    change

--- Player/States/state_generic.gd
+++ Player/States/state_generic.gd
@@ -7,5 +7,11 @@ func _init(player_reference, statemgr):
 	self.player_reference = player_reference
 	self.statemgr = statemgr
 
+func change_state_from(from_state):
+	pass
+
 func run(delta):
+	pass
+
+func state_draw():
 	pass
